4d97952a84520e408bffbb65c4afccf4b20b2164
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-23 11:15:02 -0500

    Added project details to scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,68 @@
+/*
+App Name: If  you come up with something put it here.
+
+Developers: Drew, Saranya, Tam and Anthony
+
+Tam feels most comfortable with:
+	API stuff and UI
+
+Anthony feels most comfortable with:
+	HTML/CSS
+
+Saranya feels most comfortable with:
+	HTML/CSS  I am still confusing in API concepts
+
+Drew feels most comfortable with:
+	Floater, JS and view logic outside of the api call and page/data building
+
+Project scope: Building an app that helps people leisurely browse adorable, adoptable pets and add them to a favorites list that can be referenced later.
+
+UserStory: 
+
+AS a USER, when I visit the app I am presented with a brief introduction and a button to get started
+WHEN i click on the button I am presented a window with pictures of two different animals
+THIS window has options at the top to refine my search
+THIS window has a field to enter an address and something to select the range of my search
+WHEN I click on a picture, that get added to a local storage item and saved
+THEN another set of images is presented on the screen.
+WHEN I click on a button it takes me to a page where all my selections are.
+WHEN the selections window is visible, I can click a link to where the animal is and see additional information about that pet.
+WHEN the selections window is visible, I am able to delete one pet at a time, or all of my saved pets.
+WHEN the selections window is visible, I am able to click a button to go back to selecting animals
+
+
+Acceptance Criteria:
+
+The app works and does what we say above.
+
+Backlog:
+
+
+
+New Technology: What new technology will we use? (this is part of the Acceptance Criteria)
+	Google Fonts - Saranya
+	Material Icons (part of google fonts)
+	Auto-complete library of some sort - drew
+	Maybe animation library - drew
+
+API’s: (list of API sources we will be using)
+Petfinder API
+Positionstack geocode API - Turns Address into latitude and longitude, which we will use to search for pets.
+
+
+Libraries:
+Materialize
+JQuery
+
+
+
+
+
+
+
+
+
+
+
+
+*/
