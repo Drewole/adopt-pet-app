1619b7dd102ffb50faae249f37413fedaad990b6
saranya-code <saranyadayalan28@gmail.com>
2021-03-26 14:25:12 -0500

    slideshow edited

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -106,3 +106,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//Adopt a pet toast message
+// M.toast({html: 'Successfully adopted a friend..You will receive an Email soon'})
+
+//Function for Automatic slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
