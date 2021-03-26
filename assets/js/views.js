fb20b53be687001d78c851b0e2747d2d6c2f6aa1
Drew Olsen <olsenj.andrew@gmail.com>
2021-03-26 14:11:31 -0500

    Added hidden class to saranya.css

--- assets/js/views.js
+++ assets/js/views.js
@@ -1,24 +1,21 @@
 
 //These are the window elements of the switching
-const headerEl = $("header");
-const footerEL = $("footer");
-const containerEl = $(".container");
-const welcomeViewEl = $(".welcome");//The ones below here might have to be changed
-const imagesViewEl = $(".images");
-const infoViewEl = $(".info");
+const welcomeViewEl = document.querySelector(".welcome");
+const findFriendViewEl = document.querySelector(".find-friend");
+const favoritesViewEl = document.querySelector(".favorites")
 
 
 
 
 function initialSetup() {
-	if (highScoresWindow.classList.contains("visible") || questionsWindow.classList.contains("visible")) {
+	if (imagesViewEl.classList.contains("visible") || infoViewEL.classList.contains("visible")) {
 		//Hides the scores view, hides the questions view, shows intro
-		introView();
+		viewToggle("visible","hidden","hidden");
 	}
 
 }
 /// This is the meat and potatoes
-function viewToggle(welcomeView, imagesView, infoView) {
+function viewToggle(welcomeView, findFriendView, favoritesView) {
 	if (welcomeView === "visible") {
 		welcomeViewEl.classList.add("visible");
 		welcomeViewEl.classList.remove("hidden");
@@ -27,28 +24,60 @@ function viewToggle(welcomeView, imagesView, infoView) {
 		welcomeViewEl.classList.add("hidden");
 	};
 
-	if (imagesView === "visible") {
-		imagesViewEl.classList.remove("hidden");
-		imagesViewEl.classList.add("visible");
+	if (findFriendView === "visible") {
+		findFriendViewEl.classList.add("visible");
+		findFriendViewEl.classList.remove("hidden");
 	} else {
-		imagesViewEl.classList.remove("visible");
-		imagesViewEl.classList.add("hidden");
+		findFriendViewEl.classList.remove("visible");
+		findFriendViewEl.classList.add("hidden");
 	};
 
-	if (infoView === "visible") {
-		infoViewEl.classList.remove("hidden");
-		infoViewEl.classList.add("visible");
+	if (favoritesView === "visible") {
+		favoritesViewEl.classList.remove("hidden");
+		favoritesViewEl.classList.add("visible");
 	} else {
-		infoViewEl.classList.remove("visible");
-		infoViewEl.classList.add("hidden");
+		favoritesViewEl.classList.remove("visible");
+		favoritesViewEl.classList.add("hidden");
 	};
+
+// };
+
+function welcomeView() {
+	console.log("Welcome View Showing.");
+	viewToggle("visible", "hidden", "hidden");
+};
+//Also call this when the game ends
+function findFriendView() {
+	console.log("Find Friend View Showing.");
+	//Shows the scores view, hides the questions view, hides intro
+	viewToggle("hidden", "visible", "hidden");
+
+};
+function favoritesView() {
+	console.log("Favorites View Showing.");
+
+	//Hides the scores view, hides the questions view, shows intro
+	viewToggle("hidden", "hidden", "visible");
 
 };
 
-headerEl.on("click", function (e) {
 
-})
 
-footerEl.on("click", function (e) {
+const clickContainer = document.querySelector("body");
+clickContainer.addEventListener("click", function (event) {
+	var elementClicked = event.target;
 
-})
+	console.log(elementClicked)
+	console.log($("a.home"))
+	if (elementClicked.classList.contains("welcome-link")) {
+		viewToggle("visible", "hidden", "hidden");
+	} else if (elementClicked.classList.contains("find-friend-link")) {
+		viewToggle("hidden", "visible", "hidden");
+	} else if (elementClicked.classList.contains("favorites-link")) {
+		viewToggle("hidden", "hidden", "visible");
+	};
+
+
+	//Check if the answer selected is correct - Need to make this an event bubble and catch it on the parent
+
+});
